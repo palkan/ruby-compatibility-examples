1b6664c0a7123a4b2f7c7e060e4e2a3fdb773325
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-10-04 15:20:00 +0300

    Upd at_exit example

--- examples/cli_require_at_exit.rb
+++ examples/cli_require_at_exit.rb
@@ -1,3 +1,3 @@
 res = `ruby -r #{File.join(__dir__, '../support/at_exit_typo_hook.rb')} #{File.join(__dir__, '../support/typo.rb')} 1 foo`.chomp
-raise "Assertion failed: #{res}" unless res.include?('"1, foo"')
+raise "Assertion failed: #{res}" unless res.include?('"1, foo"') && !res.include?("SyntaxError")
 
